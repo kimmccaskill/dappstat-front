5445677978f092ce380d350e01ba920a46e347be
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-16 01:17:10 -0700

    Move fetch from componentdidmount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Form from './Form.js'
-import Dashboard from './Dashboard.js'
+import Form from './Form.js';
+import Dashboard from './Dashboard.js';
+import { RotateCircleLoading } from 'react-loadingg';
 import { Route } from 'react-router-dom';
 
 class App extends React.Component {
@@ -11,50 +11,51 @@ class App extends React.Component {
     this.state = {
       address: '',
       selected_cat: '',
+      loading: false,
     }
   }
 
   submitInfo = (info) => {
     this.setState({
+      loading: true,
       address: info.address,
       selected_cat: info.selected_cat
     })
+    this.fetchDetails()
   }
 
-  // componentDidMount() {
-  //   const options = {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       ‘dapp_address’: <eth contract address>,
-  //       ‘dapp_category’: <string from dropdown>,
-  //       ‘doi’: [<contract 1>, <contract 2>, …],
-  //       ‘timeframe’: <int of days: 7, 31, 90, 365>
+  fetchDetails() {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({data: true}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
 
-  //     }),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   }
-
-  //   fetch('url', options)
-  //     .then(response => {
-  //       if(!response.ok) {
-  //           throw Error('Errow while fetching')
-  //       }
-  //       return response.json()}
-  //   )
-  //     .then(data => this.setState({data}))
-  //     .catch(err => console.log(error))
-  // }
+    fetch('https://dappstat-backend.herokuapp.com/dashboard-info-2', options)
+      .then(response => {
+        if(!response.ok) {
+            throw Error('Errow while fetching')
+        }
+        return response.json()}
+    )
+      .then(data => {
+        this.setState({loading: false,data})
+      })
+      .catch(err => console.log('error'))
+  }
   
   render() {
+    const { data, loading } = this.state;
+
     return (
       <main className='app'>
         <header className='header'>
           <h1>DappStat</h1>
         </header>
         <Route exact path='/' render={() => <Form submitInfo={this.submitInfo} />} />
-        <Route exact path='/dashboard' render={() => <Dashboard />} />
+          {loading ? <RotateCircleLoading /> : <Route exact path='/dashboard' render={() => <Dashboard results={data} />} />}
       </main>
     );
   }
